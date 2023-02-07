275ae239d0c70a32eba65ea2145e99c3af41f169
Jayson Garcia <garciajayson502@gmail.com>
2023-02-07 21:44:34 +0800

    Moved wordpress hooks from register to construct

--- src/Providers/ConverterServiceProvider.php
+++ src/Providers/ConverterServiceProvider.php
@@ -6,27 +6,33 @@ use GdWebpConverter\Converter\WebpConverter;
 
 class ConverterServiceProvider implements Provider
 {
+    public function __construct()
+    {
+        add_filter( 'wp_generate_attachment_metadata', [$this, 'gd_webp_converter'], 10, 2 );
+        add_action( 'delete_attachment', [$this, 'gd_webp_converter_delete_conversions'], 10 );
+    }
+
     public function register()
     {
-        function gd_webp_converter( $metadata, $attachment_id ) {
+        //
+    }
 
-            $gd_webp_converter = new WebpConverter( $attachment_id );
-            $gd_webp_converter->check_file_exists( $attachment_id );
-            $gd_webp_converter->check_mime_type();
-            $gd_webp_converter->create_array_of_sizes_to_be_converted( $metadata );
-            $gd_webp_converter->convert_array_of_sizes();
+    public function gd_webp_converter( $metadata, $attachment_id ) {
 
-            return $metadata;
-        }
-        add_filter( 'wp_generate_attachment_metadata', __NAMESPACE__ . '\\gd_webp_converter', 10, 2 );
+        $gd_webp_converter = new WebpConverter( $attachment_id );
+        $gd_webp_converter->check_file_exists( $attachment_id );
+        $gd_webp_converter->check_mime_type();
+        $gd_webp_converter->create_array_of_sizes_to_be_converted( $metadata );
+        $gd_webp_converter->convert_array_of_sizes();
 
-        function gd_webp_converter_delete_conversions( $attachment_id ) {
+        return $metadata;
+    }
 
-            $delete_webp_conversions = new WebpConverter( $attachment_id );
-            $delete_webp_conversions->create_array_of_sizes_to_be_deleted( $attachment_id );
-            $delete_webp_conversions->delete_array_of_sizes();
+    public function gd_webp_converter_delete_conversions( $attachment_id ) {
 
-        }
-        add_action( 'delete_attachment', __NAMESPACE__ . '\\gd_webp_converter_delete_conversions', 10 );
+        $delete_webp_conversions = new WebpConverter( $attachment_id );
+        $delete_webp_conversions->create_array_of_sizes_to_be_deleted( $attachment_id );
+        $delete_webp_conversions->delete_array_of_sizes();
+
     }
 }
--- src/Providers/GdDefaultServiceProvider.php
+++ src/Providers/GdDefaultServiceProvider.php
@@ -4,11 +4,17 @@ namespace GdWebpConverter\Providers;
 
 class GdDefaultServiceProvider implements Provider
 {
+    public function __construct()
+    {
+        add_filter( 'wp_image_editors', [$this, 'gd_webp_converter_pagely_default_to_gd'] );
+    }
+
     public function register()
     {
-        function gd_webp_converter_pagely_default_to_gd() {
-            return array( 'WP_Image_Editor_GD', 'WP_Image_Editor_Imagick' );
-        }
-        add_filter( 'wp_image_editors', __NAMESPACE__ . '\\gd_webp_converter_pagely_default_to_gd' );
+        //
+    }
+
+    public function gd_webp_converter_pagely_default_to_gd() {
+        return array( 'WP_Image_Editor_GD', 'WP_Image_Editor_Imagick' );
     }
 }
