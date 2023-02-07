11eb589c20824b3703f231e04e5a9d60ed8f963d
Jayson Garcia <53173775+hallowichig0@users.noreply.github.com>
2023-02-07 21:50:20 +0800

    Added text-domain

--- gd-webp-converter.php
+++ gd-webp-converter.php
@@ -1,7 +1,8 @@
 <?php
 /**
 * Plugin Name:  GD WebP Converter
-* Description:  After uploaded an image it will be converted to WebP format using the GD image engine. <a target="_blank" href="https://developer.wordpress.org/reference/classes/wp_image_editor_gd/">WP GD Image Engine</a> If the file is deleted form the Media Library the created WebP conversions will also be deleted.
+* Text Domain:  gd-webp-converter
+* Description:  After uploaded an image it will be converted to WebP format using the GD image engine. <a target="_blank" href="https://developer.wordpress.org/reference/classes/wp_image_editor_gd/">WP GD Image Engine</a> If the file is deleted from the Media Library the created WebP conversions will also be deleted.
 * Version:      1.0.0
 * Author:       Copia Digital
 * Author URI:   https://www.copiadigital.com/
